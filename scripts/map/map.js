6167f05447439e33b3376f524196350f922f81b2
Nuaym Syed <scienceboy75@gmail.com>
2025-02-15 14:30:15 -0800

    perf: seperate canvas for bg map for efficiency!

--- scripts/map/map.js
+++ scripts/map/map.js
@@ -6,15 +6,18 @@ class GameMap{
         //init
         this.canvas = canvas
         this.ctx = this.canvas.getContext("2d");
+        this.mapCanvas = new OffscreenCanvas(800,410);
+        this.mapCtx = this.mapCanvas.getContext("2d");
         this.map = window.mapData;
         console.log(this.map)
         this.specieTiles = new Array(800*410).fill([]);
-        this.species = []
+        this.species = [];
+
+        this.loadMap();
     }
     tick(species){
         // tick
         this.species = 0
-        let newTiles = new Array(800*410).fill([]);
         species.forEach(specie => {
             specie.tiles.forEach(tile =>{
                 forEach[tile].push(specie.id)
@@ -25,38 +28,7 @@ class GameMap{
     render(){
         //render map.
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        for(let i = 0; i < this.map.length; i+=5){
-            switch(this.map[i]){                
-                case 1: // Rainforest
-                    this.ctx.fillStyle = "rgb(0,64,0)";
-                    break;
-                case 2: // Savanna
-                    this.ctx.fillStyle = "rgb(128,128,0)";
-                    break;
-                case 3: //Desert
-                    this.ctx.fillStyle = "rgb(255,255,64)";
-                    break;
-                case 4: //Temperate forest
-                    this.ctx.fillStyle = "rgb(0,192,0)";
-                    break;
-                case 5: //Grasslands
-                    this.ctx.fillStyle = "rgb(64,64,0)";
-                    break;
-                case 6: //Tundra
-                    this.ctx.fillStyle = "rgb(64,255,255)";
-                    break;
-                case 7: //Taiga
-                    this.ctx.fillStyle = "rgb(0,64,64)";
-                    break;
-                case 8: //Ice
-                    this.ctx.fillStyle = "rgb(255,255,255)";
-                    break;
-                default: //Water
-                    this.ctx.fillStyle = "rgb(28,163,236)";
-            }
-            let pos = this.indexToCord(i);
-            this.ctx.fillRect(pos[0],pos[1], 5,1);
-        }
+        this.ctx.drawImage(this.mapCanvas, 0, 0);
     }
 
     cordToIndex(x,y){
@@ -66,6 +38,41 @@ class GameMap{
     indexToCord(index){
         let x = index % 800;
         let y = Math.floor(index/800);
-        return [x,y]
+        return [x,y];
+    }
+    loadMap(){
+        this.mapCtx.clearRect(0, 0, this.mapCanvas.width, this.mapCanvas.height);
+        for(let i = 0; i < this.map.length; i+=1){
+            switch(this.map[i]){                
+                case 1: // Rainforest
+                    this.mapCtx.fillStyle = "rgb(0,64,0)";
+                    break;
+                case 2: // Savanna
+                    this.mapCtx.fillStyle = "rgb(128,128,0)";
+                    break;
+                case 3: //Desert
+                    this.mapCtx.fillStyle = "rgb(255,255,64)";
+                    break;
+                case 4: //Temperate forest
+                    this.mapCtx.fillStyle = "rgb(0,192,0)";
+                    break;
+                case 5: //Grasslands
+                    this.mapCtx.fillStyle = "rgb(64,64,0)";
+                    break;
+                case 6: //Tundra
+                    this.mapCtx.fillStyle = "rgb(64,255,255)";
+                    break;
+                case 7: //Taiga
+                    this.mapCtx.fillStyle = "rgb(0,64,64)";
+                    break;
+                case 8: //Ice
+                    this.mapCtx.fillStyle = "rgb(255,255,255)";
+                    break;
+                default: //Water
+                    this.mapCtx.fillStyle = "rgb(28,163,236)";
+            }
+            let pos = this.indexToCord(i);
+            this.mapCtx.fillRect(pos[0],pos[1], 1,1);
+        }
     }
 }
