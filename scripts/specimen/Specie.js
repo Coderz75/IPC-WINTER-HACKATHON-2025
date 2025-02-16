6ac4a68008d20c5fb0779e40c399c729aaa7cb4d
Alexander Yu <156768800+L-A-G-B-O-T@users.noreply.github.com>
2025-02-15 20:03:58 -0800

    feat: class Plant tick function update

--- scripts/specimen/Specie.js
+++ scripts/specimen/Specie.js
@@ -1,13 +1,32 @@
 "use strict";
 
+
+
 class Plant { // Specie default class: Any subtypes of species should extend this arguably
 	static tiles = []; //we could remove this if it's unnecessary
 	static activeMembers = []; //contains references to member objects that have germinated
 	static seedMembers = []; //contains referecnes to member objects that are seeds
 	static name = "";
 	static id = 0;
-	static tick(){ //updates all members of the species
-		
+	static tick(mapCanvasContext){ //updates all members of the species, draws them as well
+		for (const aM of this.activeMembers){
+			mapCanvasContext.save();
+			mapCanvasContext.beginPath();
+			mapCanvasContext.ellipse(aM.pos.x, aM.pos.y, 5, 5, 0, 0, Math.PI*2);
+			mapCanvasContext.fillStyle = aM.color;
+			mapCanvasContext.fill();
+			mapCanvasContext.closePath();
+			mapCanvasContext.restore();
+		}
+		for (const aM of this.seedMembers){
+			mapCanvasContext.save();
+			mapCanvasContext.beginPath();
+			mapCanvasContext.ellipse(aM.pos.x, aM.pos.y, 3, 3, 0, 0, Math.PI*2);
+			mapCanvasContext.fillStyle = aM.color;
+			mapCanvasContext.fill();
+			mapCanvasContext.closePath();
+			mapCanvasContext.restore();
+		}
 	}
     constructor(parentGenome){
 		this.genome = {
@@ -23,7 +42,14 @@ class Plant { // Specie default class: Any subtypes of species should extend thi
 		this.energy = 0;
 		this.percentMaturity = 0;
 		this.randomSeed = Math.random();
-		
+
+		let percentWhole = 0;
+		for (const val of this.genome) percentWhole += val;
+		percentWhole /= 255
+			
+		this.color = `rgb(${(this.genome.competitiveness + this.genome.anchorage)/percentWhole},${(this.genome.photosynthesisRate+this.genome.waterAffinity+this.genome.waterStorage)/percentWhole},
+${(this.genome.waterStorage)/percentWhole})`;
+			
 		this.pos = { //Coordinates on the map
 			x : 0,
 			y : 0,
